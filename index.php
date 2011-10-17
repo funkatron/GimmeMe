be828f4f45b741b5f98ce1f0febabe76dcffe853
Ed Finkler <coj@funkatron.com>
2011-10-17 09:34:59 -0400

    did some stuff

--- index.php
+++ index.php
@@ -1,108 +1,10 @@
 <?php
-require('./Mustache.php');
+require('libs/Mustache.php');
+require('templates/templates.php');
 
 // config
 define('GB_CACHE_TTL', 15*60);
 define('GB_USERNAME', 'funkatron');
-
-
-// templates
-$templates = array();
-$templates['page'] = <<< EOF
-<html>
-<head>
-	<title>{{username}}</title>
-	<style>
-	@import url('main.css');
-	</style>
-</head>
-<body>
-
-<div id="container">
-	<h1>{{username}}</h1>
-
-	<ul>
-	{{#records}}
-		{{>record}}
-	{{/records}}
-	</ul>
-
-	<div class="more">View more in <a href="https://gimmebar.com/user/{{username}}">my Gimme Bar profile</a></div>
-
-</div><!-- #container -->
-
-<!-- AddThis Button BEGIN -->
-<script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=ra-4e9a50eb30ab82ff"></script>
-<!-- AddThis Button END -->
-</body>
-</html>
-EOF;
-
-$templates['partials']['record'] = <<< EOF
-	<li class="record">
-		<a name="media_hash"></a>
-		<h2 class="record-title"><a href="{{short_url}}">{{title}}</a></h2>
-		<h3 class="record-meta">
-			{{nice_date}} &mdash; <a href="{{source}}" class="record-source">source</a> &mdash; <a href="{{short_url}}" class="permalink">#</a>
-		</h3>
-		
-		<div class="record-content">
-			{{! image assets use this}}
-			{{#is_image}}{{>content_image}}{{/is_image}}
-
-			{{#is_page}}{{>content_page}}{{/is_page}}
-
-			{{#is_embed}}{{>content_embed}}{{/is_embed}}
-
-			{{#is_text}}{{>content_text}}{{/is_text}}	
-		</div>
-
-		{{#description}}
-			<div class="record-description">{{description}}</div>
-		{{/description}}
-
-		<div id="share-{{id}}" class="addthis_toolbox addthis_default_style"
-			addthis:url="{{short_url}}"
-			addthis:title="{{title}}"
-			addthis:description="{{description}}">
-			<a class="addthis_button_preferred_1"></a>
-			<a class="addthis_button_preferred_2"></a>
-			<a class="addthis_button_preferred_3"></a>
-			<a class="addthis_button_preferred_4"></a>
-			<a class="addthis_button_compact"></a>
-		</div>
-	</li>
-EOF;
-
-$templates['partials']['content_image'] = <<< EOF
-	{{#content.display}}
-		<img src="{{content.display}}">
-	{{/content.display}}
-
-	{{^content.display}}
-		<img src="{{content.full}}">
-	{{/content.display}}
-
-EOF;
-
-$templates['partials']['content_page'] = <<< EOF
-	{{#content.thumb}}
-		<a href="{{content.original}}"><img src="{{content.thumb}}"></a>
-	{{/content.thumb}}
-
-	{{^content.thumb}}
-		<a href="{{content.original}}">{{content.original}}</a>
-	{{/content.thumb}}
-EOF;
-
-$templates['partials']['content_embed'] = <<< EOF
-	{{{content.info.html}}}
-EOF;
-
-$templates['partials']['content_text'] = <<< EOF
-	{{content}}
-EOF;
-
 
 
 /************************************************************
@@ -175,7 +77,7 @@ class GimmeMe {
 }
 
 /*
- * Use this to add "lambdas" to the object
+ * Use this to add lambdas to the Mustache view object
  */
 class GimmeAsset {
 	
@@ -204,6 +106,34 @@ class GimmeAsset {
 
 	public function is_text() {
 		return ('text' === $this->asset_type);
+	}
+	
+	public function embed_html() {
+		if (!$this->is_embed()) {
+			return "not an embed!";
+		}
+		
+		if (isset($this->content->info) && isset($this->content->info->html)) {
+			return $this->content->info->html;
+		}
+
+		if (!isset($this->content->attributes)) {
+			return "no attributes!";
+		}
+
+		$tag = strtolower($this->content->tag);
+
+		// build attr string
+		$attr_str = '';
+		foreach($this->content->attributes as $key=>$val) {
+			$key = trim($key);
+			$val = trim($val);
+			$attr_str .= "{$key}=\"{$val}\" ";
+			$attr_str = trim($attr_str);
+		}
+
+		$html = "<{$tag} {$attr_str}>";
+		return $html;
 	}
 
 
